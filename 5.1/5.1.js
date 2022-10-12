dc24689ad31f6206c49318f67a31ecb333a8dcfe
Diego <113429105+trueeman@users.noreply.github.com>
2022-10-12 13:17:34 +0200

    Add files via upload

---
+++ 5.1/5.1.js
@@ -0,0 +1,14 @@
+alert("Hallo wereld.");
+
+document.getElementById("h1").style.color = "red";
+
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 * getal2;
+alert("Het antwoord is: " + som);
+
+var kleur = prompt ("Geef een Engelse kleurnaam: (red of blue", "");
+
+document.getElementById("h1").style.color = kleur;
+
+document.getElementById("p1").innerHTML = "Hello " + kleur +"!";
