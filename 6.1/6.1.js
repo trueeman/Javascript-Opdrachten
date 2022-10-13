144990df85cc49faf4437909e6ee687b92cd35e4
Diego <113429105+trueeman@users.noreply.github.com>
2022-10-13 15:39:06 +0200

    Add files via upload

---
+++ 6.1/6.1.js
@@ -0,0 +1,10 @@
+function tekst_voorbeeld() {
+    document.write("Welkom.");
+}
+
+function tekst_voorbeeld() {
+    document.getElementById("h1").innerHTML = "Inhoud aangepast!";
+    document.getElementById("h1").style.color = "red";
+}
+
+tekst_voorbeeld();
