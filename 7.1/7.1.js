34d800eb3c9e2690aec218262824989ca8856499
Diego <113429105+trueeman@users.noreply.github.com>
2022-10-20 10:20:12 +0200

    Add files via upload

---
+++ 7.1/7.1.js
@@ -0,0 +1,15 @@
+function geld() { 
+
+    let product = 60;
+    let budget= 100;
+    
+    if ( budget > product ) {
+        document.getElementById("h1").innerHTML = "Helaas, te weinig geldt";
+        document.getElementById("h1").style.color = "red";
+    }
+    else {
+        document.getElementById("h1").innerHTML = "U heeft genoeg geld!";
+        document.getElementById("h1").style.color = "green";
+    }
+}
+
