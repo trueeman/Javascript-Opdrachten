a6487564c4605fb166397229dca9bdfbca0223a3
Diego <113429105+trueeman@users.noreply.github.com>
2022-10-20 14:03:22 +0200

    Add files via upload

---
+++ 6.4/6.4.js
@@ -0,0 +1,23 @@
+function calculate(num, num2) {
+    let som = num + num2;
+    console.log(som);
+}
+calculate(2, 15);
+
+
+function multiplyByFive(getal1, getal2) {
+    let som = getal1 * getal2;
+    console.log (som);
+
+}
+multiplyByFive (14, 4);
+
+
+function calculateMinutes() {
+    let getal1  = prompt ("Schrijf een nummer hoger dan 60");
+    let getal2  = 60
+    alert (getal1 / getal2);
+    console.log(getal1 / getal2);
+
+}
+calculateMinutes();
